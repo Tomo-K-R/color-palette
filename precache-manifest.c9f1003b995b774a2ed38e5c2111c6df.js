bb9d1ecb425c07429cce2c459fede33414bc5dcf
Tomo-K-R <tomomikojo@hotmail.com>
2022-05-20 14:46:53 +0100

    Updates

---
+++ precache-manifest.c9f1003b995b774a2ed38e5c2111c6df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd5831f070c9883dd734",
+    "url": "/color-palette/static/css/main.0d626cef.chunk.css"
+  },
+  {
+    "revision": "bd5831f070c9883dd734",
+    "url": "/color-palette/static/js/main.f37ed51d.chunk.js"
+  },
+  {
+    "revision": "5d67c3cd455a1d0948f4",
+    "url": "/color-palette/static/js/runtime~main.56200f79.js"
+  },
+  {
+    "revision": "4d35953c7bfb730e9e48",
+    "url": "/color-palette/static/js/2.2fadb762.chunk.js"
+  },
+  {
+    "revision": "0903a2acb8e1c8e4f5a685af9804f100",
+    "url": "/color-palette/index.html"
+  }
+];
